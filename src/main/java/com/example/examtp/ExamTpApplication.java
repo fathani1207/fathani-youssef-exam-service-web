588d3c4eb2bc4ffed62f199a875317a313f91e4a
fathani1207 <youssef.fathani1207@gmail.com>
2025-10-25 22:00:29 +0200

    initialisation de projet

---
+++ src/main/java/com/example/examtp/ExamTpApplication.java
@@ -0,0 +1,13 @@
+package com.example.examtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamTpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamTpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/examtp/ExamTpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examtp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamTpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
