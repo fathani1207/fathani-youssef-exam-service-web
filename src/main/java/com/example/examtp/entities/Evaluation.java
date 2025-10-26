5f95fd5dfd1390a349f0fcec26953ed9e68695b7
fathani1207 <youssef.fathani1207@gmail.com>
2025-10-26 02:45:03 +0200

    creation des entites

---
+++ src/main/java/com/example/examtp/entities/Evaluation.java
@@ -0,0 +1,36 @@
+package com.example.examtp.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@Entity
+@Table(name = "evaluations")
+@Getter
+@Setter
+public class Evaluation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "author", length = 50)
+    private String author;
+
+    @Column(name = "content", length = 255)
+    private String content;
+
+    @Column(name = "note")
+    private int note;
+
+    @ElementCollection
+    @CollectionTable(name = "evaluation_images", joinColumns = @JoinColumn(name = "evaluation_id"))
+    @Column(name = "evaluation_image_url", length = 500)
+    private List<String> evaluationImagesUrls;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+}
---
+++ src/main/java/com/example/examtp/entities/Restaurant.java
@@ -0,0 +1,30 @@
+package com.example.examtp.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@Entity
+@Table(name = "restaurants")
+@Getter
+@Setter
+public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name", length = 90)
+    private String name;
+
+    @Column(name = "address", length = 255)
+    private String address;
+
+    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL)
+    private List<Evaluation> evaluations;
+
+    @Column(name = "restaurant_image", length = 500)
+    private String restaurantImageUrl;
+}
